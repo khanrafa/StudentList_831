5bac046f60270f8de910b9b290468a9c0ec8b3a0
RafaeKhan <khanrafa@sheridancollege.ca>
2021-01-31 18:38:25 -0500

    added address

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -7,10 +7,21 @@ package studentlist;
 public class Student {
 
     private String name;
+    private String address;
 
     public Student(String name) {
         this.name = name;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
     
     public String getName() {
         return name;
